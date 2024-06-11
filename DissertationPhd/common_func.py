3edb62526c87f16835780aee740b24ae0e16ebb0
PernatiyZybr <orlov.vv@phystech.edu>
2024-06-11 17:05:10 +0300

    тык тыктык

---
+++ DissertationPhd/common_func.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def quart2dcm(L):
+    """Функция ищет матрицу поворота из кватерниона поворота; \n
+    Кватернион L передаётся вектором длины 4; \n
+    Возвращает матрицу 3х3."""
+    w, x, y, z = L
+    A = np.eye(3)
+    A[0][0] = 1 - 2 * y ** 2 - 2 * z ** 2
+    A[0][1] = 2 * x * y + 2 * z * w
+    A[0][2] = 2 * x * z - 2 * y * w
+    A[1][0] = 2 * x * y - 2 * z * w
+    A[1][1] = 1 - 2 * x ** 2 - 2 * z ** 2
+    A[1][2] = 2 * y * z + 2 * x * w
+    A[2][0] = 2 * x * z + 2 * y * w
+    A[2][1] = 2 * y * z - 2 * x * w
+    A[2][2] = 1 - 2 * x ** 2 - 2 * y ** 2
+    return A
+
+def q_dot(L1, L2):
+    """Функция является кватернионным умножением; \n
+    Кватернион L1,L2 передаются векторами длины 4; \n
+    Возвращает кватернион L[0]..L[3]."""
+    return np.array([L1[0] * L2[0] - L1[1] * L2[1] - L1[2] * L2[2] - L1[3] * L2[3],
+                     L1[0] * L2[1] + L1[1] * L2[0] + L1[2] * L2[3] - L1[3] * L2[2],
+                     L1[0] * L2[2] + L1[2] * L2[0] + L1[3] * L2[1] - L1[1] * L2[3],
+                     L1[0] * L2[3] + L1[3] * L2[0] + L1[1] * L2[2] - L1[2] * L2[1]])
+
+def get_U_S(A, w_0, t):
+    from sympy import sin, cos, Matrix
+    U = Matrix([[0, 1, 0],
+                [0, 0, 1],
+                [1, 0, 0]]) @ \
+        Matrix([[cos(t * w_0), sin(t * w_0), 0],
+                [-sin(t * w_0), cos(t * w_0), 0],
+                [0, 0, 1]])
+    S = A @ U.T
+    return U, S
+
+# ----------------------------------------- ПЕРЕХОДЫ МЕЖДУ СИСТЕМАМИ КООРДИНАТ
+def i_o(self, a, U=None):
+    """Инерциальная -> Орбитальная"""
+    a_np = np.array(a)
+    U = self.U if (U is None) else U
+    return U @ a_np - np.array([0, 0, self.Re])
+
+def o_i(self, a, U=None):
+    """Орбитальная -> Инерциальная"""
+    a_np = np.array(a)
+    U = self.U if (U is None) else U
+    return U.T @ (a_np + np.array([0, 0, self.Re]))

fbf81daefc921d48e72e18922c1319edca552cf0
PernatiyZybr <orlov.vv@phystech.edu>
2024-11-28 03:03:35 +0300

    тык тыктык

--- DissertationPhd/common_func.py
+++ DissertationPhd/common_func.py
@@ -1,76 +1,80 @@
+from __init__ import *
+from sympy import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-# ----------------------------------------- ТРИВИАЛЬНЫЕ ФУНКЦИИ
-def clip(a: float, bot: float, top: float) -> float:
-    if a < bot:
-        return bot
-    if a > top:
-        return top
-    return a
+# >>>>>>>>>>>>>>>>>>>> OBSERVABILITY <<<<<<<<<<<<<<<<<<<< (функции)
+def save_reports(report_list: list, filename: str) -> None:
+    """Сохраняет в компактном виде несколько выводов в один
+    :param report_list: Строки вывода, запрашиваемые к объединению
+    :param filename: Название текстовика, в котором будет храниться информация"""
+    common_report = report_list[0]
+    for i in range(len(report_list) - 1):
+        common_report += ("\n" + "-" * 100)*2 + "\n" + report_list[i + 1]
+    f = open("storage/observability_reoprt_" + filename + ".txt", "w")
+    f.write(common_report)
+    f.close()
 
-# ----------------------------------------- ФУНКЦИИ МАТРИЦ И ВЕКТОРОВ
-def quart2dcm(L):
-    """Функция ищет матрицу поворота из кватерниона поворота; \n
-    Кватернион L передаётся вектором длины 4; \n
-    Возвращает матрицу 3х3."""
-    w, x, y, z = L
-    A = np.eye(3)
-    A[0][0] = 1 - 2 * y ** 2 - 2 * z ** 2
-    A[0][1] = 2 * x * y + 2 * z * w
-    A[0][2] = 2 * x * z - 2 * y * w
-    A[1][0] = 2 * x * y - 2 * z * w
-    A[1][1] = 1 - 2 * x ** 2 - 2 * z ** 2
-    A[1][2] = 2 * y * z + 2 * x * w
-    A[2][0] = 2 * x * z + 2 * y * w
-    A[2][1] = 2 * y * z - 2 * x * w
-    A[2][2] = 1 - 2 * x ** 2 - 2 * y ** 2
-    return A
+def read_reports(filename: str) -> str:
+    """Считывает выводы, сохранённые в один файл
+    :param filename: Название текстовика, в котором хранится информация"""
+    f = open("storage/observability_reoprt_" + filename + ".txt", "r")
+    common_report = f.read()
+    f.close()
+    return common_report
 
-def q_dot(L1, L2):
-    """Функция является кватернионным умножением; \n
-    Кватернион L1,L2 передаются векторами длины 4; \n
-    Возвращает кватернион L[0]..L[3]."""
-    return np.array([L1[0] * L2[0] - L1[1] * L2[1] - L1[2] * L2[2] - L1[3] * L2[3],
-                     L1[0] * L2[1] + L1[1] * L2[0] + L1[2] * L2[3] - L1[3] * L2[2],
-                     L1[0] * L2[2] + L1[2] * L2[0] + L1[3] * L2[1] - L1[1] * L2[3],
-                     L1[0] * L2[3] + L1[3] * L2[0] + L1[1] * L2[2] - L1[2] * L2[1]])
+def print_spectrm_rank(J_numb):
+    for tol in [1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10, 1e-11, 1e-12, 1e-13, 1e-14, 1e-15, 1e-20]:
+        print(f"Ранг матрицы: {np.linalg.matrix_rank(J_numb, tol=tol)} (tol={tol})")
 
-def get_U_S(A, w_0, t, ECCENTRICITY = None, INCLINATION = None):
-    from sympy import sin, cos, Matrix, atan, sqrt, tan
-    if ECCENTRICITY is None and INCLINATION is None:
-        U = Matrix([[0, 1, 0],
-                    [0, 0, 1],
-                    [1, 0, 0]]) @ \
-            Matrix([[cos(t * w_0), sin(t * w_0), 0],
-                    [-sin(t * w_0), cos(t * w_0), 0],
-                    [0, 0, 1]])
-    else:
-        e = 0 if ECCENTRICITY is None else ECCENTRICITY
-        i = 0 if INCLINATION is None else INCLINATION
-        E = t * w_0  # Эксцентрическая аномалия
-        f = 2 * atan(sqrt((1 + e) / (1 - e)) * tan(E / 2))  # Истинная аномалия
-        U = Matrix([[0, 1, 0],  # Поворот к экваториальной плоскости
-                    [0, 0, 1],
-                    [1, 0, 0]]) @ \
-            Matrix([[cos(f), sin(f), 0],  # Разница между истинной аномалией и местной
-                    [-sin(f), cos(f), 0],
-                    [0, 0, 1]]) @ \
-            Matrix([[1, 0, 0],
-                    [0, cos(i), sin(i)],  # Поворот к плоскости орбиты
-                    [0, -sin(i), cos(i)]])
-    S = A @ U.T
-    return U, S
+# >>>>>>>>>>>>>>>>>>>> OBSERVABILITY <<<<<<<<<<<<<<<<<<<< (переменные)
+def get_state_vector(func, obj: str, n: int = 1):
+    kw = {'n': 3, 'numb': False}
+    if func == kf.get_func:
+        kw['t'] = t
+    return ([func(name=f'r_{i}^{obj}', **kw) for i in range(n)],
+            [func(name=f'v_{i}^{obj}', **kw) for i in range(n)],
+            [kf.vec2quat(func(name=f'q_{i}^{obj}', **kw)) for i in range(n)],
+            [func(name=f'ω_{i}^{obj}', **kw) for i in range(n)])
 
-# ----------------------------------------- ПЕРЕХОДЫ МЕЖДУ СИСТЕМАМИ КООРДИНАТ
-def i_o(self, a, U=None):
-    """Инерциальная -> Орбитальная"""
-    a_np = np.array(a)
-    U = self.U if (U is None) else U
-    return U @ a_np - np.array([0, 0, self.Re])
+def init_symbol_params():
+    o = kf.init()
 
-def o_i(self, a, U=None):
-    """Орбитальная -> Инерциальная"""
-    a_np = np.array(a)
-    U = self.U if (U is None) else U
-    return U.T @ (a_np + np.array([0, 0, self.Re]))
+    # Запись численных значений
+    num_params = {'h': o.v.HEIGHT, 
+                  'r0': o.v.ORBIT_RADIUS,
+                  'ω0': o.v.W_ORB,
+                  'v0': o.v.V_ORB,
+                  'μ': o.v.MU,
+                  'Jd': o.f.J,
+                  'md': o.f.mass, 
+                  'Cd': o.f.c_resist,
+                  'sd': o.f.size,
+                  'Jc': o.c.J,
+                  'mc': o.c.mass, 
+                  'Cc': o.c.c_resist,
+                  'sc': o.c.size,
+                  'ρ': kf.get_atm_params(v=o.v, h=o.v.HEIGHT)[0],
+                 }
+    print(f"Высота орбиты: {int(o.v.HEIGHT // 1e3)} км\nПериод орбиты: {round((2*np.pi/o.v.W_ORB) / 3600, 2)} часов\nПлотность атмосферы: {num_params['ρ']} кг/м³")
+
+    # Подстановка символьных переменных
+    t, ω, μ, ρ, r_orb, v_orb = var("t ω_0 μ ρ r_0 v_0")
+    o.v.ORBIT_RADIUS = r_orb
+    o.v.V_ORB = v_orb
+    o.v.W_ORB = ω
+    o.v.MU = μ
+    
+    # Те, которые не передаются
+    o.v.dT = var('dt')
+    
+    J = [kf.get_vars("J^d", 3, numb=False), kf.get_vars("J^c", 3, numb=False)]
+    o.f.J = diag(J[0][0], J[0][1], J[0][2])
+    o.c.J = diag(J[1][0], J[1][1], J[1][2])
+    o.f.mass, o.c.mass = var('m_d m_c')
+    o.f.c_resist, o.c.c_resist = var('C_d C_c')
+    o.f.size = kf.get_vars(name='s^d', n=2)
+    o.c.size = kf.get_vars(name='s^c', n=2)
+
+    return o, num_params, t, ω, μ, ρ, r_orb, v_orb
+    
